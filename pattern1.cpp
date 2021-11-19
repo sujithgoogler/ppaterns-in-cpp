48d5cc74d9b895e96c331313ff8e45a3583da82f
SUJITHGOOGLER <59825563+sujithgoogler@users.noreply.github.com>
2021-11-19 09:02:18 +0530

    Create pattern1.cpp

---
+++ pattern1.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define l "\n"
+void pattern1(int n)
+{
+	for(int row=1;row<=n;row++)
+	{
+		for(int col=1;col<=row;col++)
+		{
+		   cout<<"*";
+		}
+		cout<<l;
+	}
+	
+}
+
+
+
+
+
+int main() {
+	int n;
+	cin>>n;
+pattern1(n);
+
+	return 0;
+}
